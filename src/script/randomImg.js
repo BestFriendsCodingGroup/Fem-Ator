0cb680113cabb84db24834f1c53e2c937e310c43
Funko <93531050+FunkoDev@users.noreply.github.com>
2023-03-02 10:40:42 +0000

    YEah

--- src/script/randomImg.js
+++ src/script/randomImg.js
@@ -1,5 +1,7 @@
 window.onload = choosePic;
 
+var dir = "public/hero/";
+
 function choosePic() {
-     document.getElementById("fukinimg").src = "public/hero/" + Math.floor(Math.random() * 2) +".png";
+     document.getElementById("fukinimg").src = dir + Math.floor(Math.random() * 2) +".png";
 }
